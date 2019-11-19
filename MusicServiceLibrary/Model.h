6f4618f06235cafe63fef2a9ab92c69c2d1f1049
gmenchetti <gmenchetti@github.com>
2019-11-19 12:37:38 -0600

    First commit

---
+++ MusicServiceLibrary/Model.h
@@ -0,0 +1,36 @@
+//
+// Created by Guglielmo Menchetti on 17/11/2018.
+//
+
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "LinkedList.h"
+#include <stdio.h>
+
+class Model {
+public:
+    Model(int nSongs);
+    ~Model();
+
+    void listSongs();
+    void edit(int index);
+    void saveClips();
+    void createNewClip();
+    void deepCopy();
+    void changePrice(float newPrice);
+    void printSong(MusicClipPtr *mptr);
+    void readFromFile();
+    int execute(std::string inst);
+private:
+    int numberOfSongs;
+    int activePointers[3] = {-1, -1, -1};
+    MusicClipPtr* currentMusicPtr;
+    LinkedList songList;
+    MusicClipPtr* getPointer(int index);
+    void removeCurrentFiles();
+    std::string parseSingleString(std::string oldString);
+    std::vector<std::string> parsePeople(std::string oldString);
+};
+
+#endif //MODEL_H
